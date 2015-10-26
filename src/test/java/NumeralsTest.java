9853c0f9d969ba8c68624a118f9d7ceb19782e99
Lauri Hahne <lauri.hahne@gmail.com>
2015-10-26 09:40:08 +0200

    add java boilerplate

---
+++ src/test/java/NumeralsTest.java
@@ -0,0 +1,14 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Created by lauri.hahne on 26/10/15.
+ */
+
+public class NumeralsTest {
+
+    @Test
+    public void testFoo() {
+        Assert.assertTrue(true);
+    }
+}
